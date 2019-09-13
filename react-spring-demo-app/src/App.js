582929a463d2dbc14f016b5194c983ed3c7682c1
olympia <owojcik7@gmail.com>
2019-09-12 21:10:22 -0400

    Create Component and add animation for delay and duration to make it fade in slower

--- react-spring-demo-app/src/App.js
+++ react-spring-demo-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Component1 from "./components/Component1";
+import Component2 from "./components/Component2";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Component1 />
+      <Component2 />
     </div>
   );
 }
---
+++ react-spring-demo-app/src/components/Component2.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Spring } from "react-spring/renderprops";
+//class based b/c it'll have button to toggle component3 so pass props up
+
+class Component2 extends React.Component {
+  render() {
+    return (
+      <Spring
+        from={{ opacity: 0 }}
+        to={{ opacity: 1 }}
+        config={{ delay: 1000, duration: 1000 }}
+      >
+        {props => (
+          <div style={props}>
+            <div style={c2style}>
+              <h1>Component 1</h1>
+              <p>
+                Incididunt et qui occaecat tempor duis ut et elit reprehenderit
+                nostrud. Officia adipisicing veniam quis fugiat. Tempor nostrud
+                eu enim tempor ut veniam veniam. Velit commodo reprehenderit
+                dolor irure mollit veniam magna. Laboris excepteur ipsum anim
+                Lorem tempor sit ut occaecat.
+              </p>
+            </div>
+          </div>
+        )}
+      </Spring>
+    );
+  }
+}
+export default Component2;
+
+const c2style = {
+  background: "slateblue",
+  color: "white",
+  padding: "1.5rem"
+};
