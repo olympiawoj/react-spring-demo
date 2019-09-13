54eeb00fb0335eec9181b1502549b1bc23d5025a
olympia <owojcik7@gmail.com>
2019-09-12 21:00:58 -0400

    Add Spring animation to Component1- opacity and slide down

--- react-spring-demo-app/src/components/Component1.js
+++ react-spring-demo-app/src/components/Component1.js
@@ -1,16 +1,28 @@
 import React from "react";
 
+import { Spring } from "react-spring/renderprops";
+
 export default function Component1() {
   return (
-    <div style={c1style}>
-      <h1>Component 1</h1>
-      <p>
-        Incididunt et qui occaecat tempor duis ut et elit reprehenderit nostrud.
-        Officia adipisicing veniam quis fugiat. Tempor nostrud eu enim tempor ut
-        veniam veniam. Velit commodo reprehenderit dolor irure mollit veniam
-        magna. Laboris excepteur ipsum anim Lorem tempor sit ut occaecat.
-      </p>
-    </div>
+    <Spring
+      from={{ opacity: 0, marginTop: -500 }}
+      to={{ opacity: 1, marginTop: 0 }}
+    >
+      {props => (
+        <div style={props}>
+          <div style={c1style}>
+            <h1>Component 1</h1>
+            <p>
+              Incididunt et qui occaecat tempor duis ut et elit reprehenderit
+              nostrud. Officia adipisicing veniam quis fugiat. Tempor nostrud eu
+              enim tempor ut veniam veniam. Velit commodo reprehenderit dolor
+              irure mollit veniam magna. Laboris excepteur ipsum anim Lorem
+              tempor sit ut occaecat.
+            </p>
+          </div>
+        </div>
+      )}
+    </Spring>
   );
 }
 
