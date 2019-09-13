238a4f4cdc909856ef87f1c287276797aeac6d34
olympia <owojcik7@gmail.com>
2019-09-12 20:25:02 -0400

    Create component1 and apply styles

---
+++ react-spring-demo-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Component1 from "./components/Component1";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Component1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-spring-demo-app/src/components/Component1.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Component1() {
+  return (
+    <div style={c1style}>
+      <h1>Component 1</h1>
+      <p>
+        Incididunt et qui occaecat tempor duis ut et elit reprehenderit nostrud.
+        Officia adipisicing veniam quis fugiat. Tempor nostrud eu enim tempor ut
+        veniam veniam. Velit commodo reprehenderit dolor irure mollit veniam
+        magna. Laboris excepteur ipsum anim Lorem tempor sit ut occaecat.
+      </p>
+    </div>
+  );
+}
+
+const c1style = {
+  background: "steelblue",
+  color: "white",
+  padding: "1.5rem"
+};
